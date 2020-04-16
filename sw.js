128223c2acd047766c4a9ad693e0631cff974c1f
unknown <li3151@snet.isid.co.jp>
2020-04-16 10:26:49 +0900

    インストール確認2

--- sw.js
+++ sw.js
@@ -4,9 +4,9 @@
 // キャッシュ名とキャッシュファイルの指定
 var CACHE_NAME = 'pwa-sample-caches';
 var urlsToCache = [
-	'./pwa/',
-	'./pwa/css/style.css',
-	'./pwa/drawer.js'
+	'./index.html',
+	'./css/style.css',
+	'./drawer.js'
 ];
 
 // インストール処理
